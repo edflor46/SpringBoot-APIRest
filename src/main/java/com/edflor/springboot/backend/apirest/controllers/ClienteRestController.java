e6a41b4e41d482c5251b26292a5b4f617748f189
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-16 16:23:26 -0600

    Metodos Create y GetById agregados

--- src/main/java/com/edflor/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/edflor/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,11 +3,10 @@ package com.edflor.springboot.backend.apirest.controllers;
 import com.edflor.springboot.backend.apirest.models.entity.Cliente;
 import com.edflor.springboot.backend.apirest.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = { "http://localhost:4200", "*" })
@@ -21,6 +20,18 @@ public class ClienteRestController {
     @GetMapping("/clientes")
     public List<Cliente> index() {
         return clienteService.findAll();
+    }
+
+    @GetMapping("/cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Cliente show(@PathVariable Long id){
+        return clienteService.findById(id);
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente){
+    return clienteService.save(cliente);
     }
 
 }
