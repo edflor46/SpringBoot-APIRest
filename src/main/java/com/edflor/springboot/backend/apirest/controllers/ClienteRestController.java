08d3e616f2f97ad2aa097f4c2d4707dad97dacd5
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-16 14:33:07 -0600

    @CrossOrigin agregado

--- src/main/java/com/edflor/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/edflor/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,12 +3,14 @@ package com.edflor.springboot.backend.apirest.controllers;
 import com.edflor.springboot.backend.apirest.models.entity.Cliente;
 import com.edflor.springboot.backend.apirest.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
