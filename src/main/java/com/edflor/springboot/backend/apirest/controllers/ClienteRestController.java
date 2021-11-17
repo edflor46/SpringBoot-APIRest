24a813cd35edb54167ef0ae23a085ef4ec729b79
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-17 14:22:04 -0600

    Manejo de errores - metodo update

--- src/main/java/com/edflor/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/edflor/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = { "http://localhost:4200", "*" })
+@CrossOrigin(origins = {"http://localhost:4200", "*"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
@@ -28,20 +28,20 @@ public class ClienteRestController {
 
     @GetMapping("/cliente/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<?> show(@PathVariable Long id){
+    public ResponseEntity<?> show(@PathVariable Long id) {
 
         Cliente cliente = null;
         Map<String, Object> response = new HashMap<>();
         try {
             cliente = clienteService.findById(id);
-        }catch (DataAccessException e){
+        } catch (DataAccessException e) {
             response.put("mensaje", "Error al realizar la peticion");
             response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
         }
 
 
-        if (cliente == null){
+        if (cliente == null) {
             response.put("mensaje", "El cliente con el ID: ".concat(id.toString().concat(" no es un id valido")));
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
         }
@@ -49,13 +49,13 @@ public class ClienteRestController {
     }
 
     @PostMapping("/clientes")
-    public ResponseEntity<?> create(@RequestBody Cliente cliente){
+    public ResponseEntity<?> create(@RequestBody Cliente cliente) {
 
         Cliente clienteNew = null;
         Map<String, Object> response = new HashMap<>();
         try {
             clienteNew = clienteService.save(cliente);
-        }catch (DataAccessException e){
+        } catch (DataAccessException e) {
             response.put("mensaje", "Error al realizar el insert la peticion");
             response.put("error", e.getMessage().concat(" :").concat(e.getMostSpecificCause().getMessage()));
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -64,24 +64,43 @@ public class ClienteRestController {
         response.put("mensaje", "El cliente ha sido agregado con exito");
         response.put("cliente", clienteNew);
 
-    return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/cliente/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public  Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
+    public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+        Map<String, Object> response = new HashMap<>();
         Cliente clienteActual = clienteService.findById(id);
-        clienteActual.setApellido(cliente.getApellido());
-        clienteActual.setNombre(cliente.getNombre());
-        clienteActual.setEmail(cliente.getEmail());
+        Cliente clienteUpdated = null;
 
-        return clienteService.save(clienteActual);
+        if (clienteActual == null) {
+            response.put("mensaje", "Error: no se pudo editar, el cliente con el ID: ".concat(id.toString().concat(" no es un id valido")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            clienteActual.setApellido(cliente.getApellido());
+            clienteActual.setNombre(cliente.getNombre());
+            clienteActual.setEmail(cliente.getEmail());
+            clienteActual.setCreateAt(cliente.getCreateAt());
+            clienteUpdated = clienteService.save(clienteActual);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al actualizar el cliente");
+            response.put("error", e.getMessage().concat(" :").concat(e.getMostSpecificCause().getMessage()));
+            return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El cliente ha sido actualizado con exito");
+        response.put("cliente", clienteUpdated);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 
     }
 
     @DeleteMapping("/cliente/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         clienteService.delete(id);
     }
 }
